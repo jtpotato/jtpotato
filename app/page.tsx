f45d3a86cc267ff245063c60274e8c16cec0f8de
jtpotato <58995538+jtpotato@users.noreply.github.com>
2022-11-11 20:12:01 +1100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import "../styles/globals.css";
+import PageRails from "./pageRails";
+
+const Home = () => {
+  return (
+    <>
+      <PageRails>
+        <h1>{"Hi, I'm Joel"}</h1>
+      </PageRails>
+    </>
+  );
+};
+
+export default Home;
---
+++ app/pageRails.tsx
@@ -0,0 +1,9 @@
+const PageRails = (props: { children: any }) => {
+  return (
+    <>
+      <div>{props.children}</div>
+    </>
+  );
+};
+
+export default PageRails;
