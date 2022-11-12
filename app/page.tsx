74e069e7b83bf6c4285391dda0e22cc999ab5935
jtpotato <58995538+jtpotato@users.noreply.github.com>
2022-11-12 17:13:34 +1100

    create extra misc links

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 
+import Link from "next/link";
+
 /* eslint-disable react/no-unescaped-entities */
 function Home() {
   return (
@@ -22,8 +24,34 @@ function Home() {
             <p className="font-bold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
               GitHub Stats
             </p>
-            <img src="https://github-readme-stats.vercel.app/api?username=jtpotato&show_icons=true&theme=dark&count_private=true" alt="Github Stats"></img>
-            <img src="https://github-readme-stats.vercel.app/api/top-langs?username=jtpotato&show_icons=true&theme=dark&layout=compact" alt="Most Used Languages"></img>
+            <img
+              src="https://github-readme-stats.vercel.app/api?username=jtpotato&show_icons=true&theme=dark&count_private=true"
+              alt="Github Stats"
+            ></img>
+            <img
+              src="https://github-readme-stats.vercel.app/api/top-langs?username=jtpotato&show_icons=true&theme=dark&layout=compact"
+              alt="Most Used Languages"
+            ></img>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center h-screen">
+          <div className="grid gap-4">
+            <p className="font-bold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              More
+            </p>
+            <div>
+              <Link
+                href={
+                  "https://open.spotify.com/user/p2u0lsb3vojptkp3g4vy5gp9p?si=dd6eb571693b4d08"
+                }
+              >
+                <p className="text-white underline">Spotify</p>
+              </Link>
+              <Link href="https://www.16personalities.com/profiles/a6937617c0cbe">
+                <p className="text-white underline">16personalities</p>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
