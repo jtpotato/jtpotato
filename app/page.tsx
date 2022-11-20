dbbba24b22b8a39667446d5d022ff353716265a5
jtpotato <58995538+jtpotato@users.noreply.github.com>
2022-11-20 13:03:08 +1100

    alt text

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import NameCard from "./nameCard";
-import { Text, Container, Card, Grid, Image } from "@nextui-org/react";
+import { Text, Container, Card, Grid, Image, Link } from "@nextui-org/react";
 
 /* eslint-disable react/no-unescaped-entities */
 function Home() {
@@ -17,23 +17,35 @@ function Home() {
         >
           Projects
         </Text>
-        <Card css={{ p: "$6", mw: "400px" }}>
-          <Card.Header>
-            <Grid.Container>
-              <Grid xs={12}>
-                <Text h5 css={{ lineHeight: "$xs" }}>
-                  YouTube Experiments
-                </Text>
-              </Grid>
-              <Grid xs={12}>
-                <Text color="secondary">ca. 2020</Text>
-              </Grid>
-            </Grid.Container>
-          </Card.Header>
-          <Card.Body>
-            <Image src="/public/assets/home/ytexperiments.png"></Image>
-          </Card.Body>
-        </Card>
+        <Grid.Container>
+          <Grid xs={4}>
+            <Card>
+              <Card.Header>
+                <Grid.Container>
+                  <Grid xs={12}>
+                    <Text h5 css={{ lineHeight: "$xs" }}>
+                      YouTube Experiments
+                    </Text>
+                  </Grid>
+                  <Grid xs={12}>
+                    <Text color="secondary">ca. 2020</Text>
+                  </Grid>
+                </Grid.Container>
+              </Card.Header>
+              <Card.Body>
+                <Image src="/assets/home/ytexperiments.png" css={{mh: "20em"}} alt="Screenshot of YouTube Experiments repl.it page"></Image>
+              </Card.Body>
+              <Card.Footer>
+                <Link
+                  target="_blank"
+                  href="https://replit.com/@JTpotato/YTExperiments"
+                >
+                  See it in action on repl.it
+                </Link>
+              </Card.Footer>
+            </Card>
+          </Grid>
+        </Grid.Container>
       </Container>
     </>
   );
