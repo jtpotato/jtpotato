f8b9dd8c3a457b325cc3cdc20526bec29c8154ca
Joel <58995538+jtpotato@users.noreply.github.com>
2023-05-20 21:02:22 +1000

    turn this into a vercel rewrite page

--- src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<div class="w-screen h-screen flex items-center justify-center bg-stone-900">
-	<h1 class="font-bold text-8xl text-white">
-		Hi, I'm
-		<h1 class="inline text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-blue-500">Joel</h1>
-	</h1>
-</div>
